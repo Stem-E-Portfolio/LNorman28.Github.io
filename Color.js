b37d6e0f3b27395915e1ba9ce52edc7ea5152d6a
Stem-E-Portfolio <45765549+Stem-E-Portfolio@users.noreply.github.com>
2018-12-18 17:35:05 -0700

    Pc upload

--- Color.js
+++ Color.js
@@ -1,4 +1,4 @@
-function random_bg_color() {
+﻿function random_bg_color() {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
